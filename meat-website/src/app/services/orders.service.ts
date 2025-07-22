14a654d53752f8e07bc2bedf520a56e125a944b5
stacztech <stacztech@gmail.com>
2025-07-22 23:04:00 +0530

    Update orders.service.ts

--- meat-website/src/app/services/orders.service.ts
+++ meat-website/src/app/services/orders.service.ts
@@ -50,7 +50,7 @@ export interface OrderTracking {
   providedIn: 'root'
 })
 export class OrdersService implements OnDestroy {
-  private apiUrl = 'http://localhost:9000/api/orders';
+  private apiUrl = 'https://ponsbackend.onrender.com/api/orders';
 
   constructor(private authService: AuthService, private http: HttpClient) {
     // No need to subscribe to user changes for backend
